47f941b94e7dc32058040ff99eeaa22507cd57c2
Chris Farnham <cfarnham@artisight.com>
2024-12-31 10:09:20 -0500

    made it fancier

--- analyze.py
+++ analyze.py
@@ -3,32 +3,49 @@ import unicodedata
 import adif_io
 import matplotlib.pyplot as plt
 import polars as pl
+import argparse
 
-if __name__ == '__main__':
+def plot_mode_distribution(df):
+  mode_counts = df["MODE"].value_counts()
+  print(mode_counts)
+  total_mode = df["MODE"].count()
+  mode_counts = mode_counts.with_columns((mode_counts["count"] / total_mode * 100).alias("MODE_PERCENTAGE"))
+
+  print(mode_counts)
 
 
-  calls = {}
+  # Extract data for the pie chart
+  labels = mode_counts['MODE'].to_list()
+  sizes = mode_counts['MODE_PERCENTAGE'].to_list()
+
+  # Create a pie chart
+  plt.figure(figsize=(8, 6))
+  plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+  plt.title('W1YTQ Operating Modes in 2024')
+  plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+  plt.show()
+
+def main():
+
+  # Set up argument parser
+  parser = argparse.ArgumentParser(description="Analyze ham radio data")
+  report_choices = ['columns', 'mode_distribution']
+  parser.add_argument('report', choices=report_choices, default='columns', nargs='?', help='Type of report to generate')
+
+  # Parse command-line arguments
+  args = parser.parse_args()
+
   with open("./w1ytq.349397.20241231142157.adi", 'r', encoding = "ISO-8859-1") as rf:
     content = ''.join(rf.readlines())
     qsos, header = adif_io.read_from_string(content)
     df = pl.DataFrame([{k:v for k,v in q.items()} for q in qsos])
 
-    print(df.columns)
-    mode_counts = df["MODE"].value_counts()
-    print(mode_counts)
-    total_mode = df["MODE"].count()
-    mode_counts = mode_counts.with_columns((mode_counts["count"] / total_mode * 100).alias("MODE_PERCENTAGE"))
-
-    print(mode_counts)
+    # Execute the specified report
+    if args.report == 'mode_distribution':
+        plot_mode_distribution(df)
+    else:
+      print(df.columns)
 
 
-    # Extract data for the pie chart
-    labels = mode_counts['MODE'].to_list()
-    sizes = mode_counts['MODE_PERCENTAGE'].to_list()
-
-    # Create a pie chart
-    plt.figure(figsize=(8, 6))
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-    plt.title('W1YTQ Operating Modes in 2024')
-    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    plt.show()
+if __name__ == '__main__':
+  main()
